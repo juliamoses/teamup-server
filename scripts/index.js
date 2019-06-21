e145b50bfeed2727adddfa80e6fc49eecb57c5cc
David Eastmond <davebuck1981@gmail.com>
2019-06-21 19:29:34 -0400

    email validation

--- scripts/index.js
+++ scripts/index.js
@@ -70,23 +70,25 @@ function increaseInputs() {
   // console.log(cols[0].children[0].value);
   // console.log(cols[0].children[1].value);
   $("#sharee-count-label").text(`(${cols.length}) chunks`);
-  iterateThroughValues(cols)
 }
 
 function decreaseInputs() {
   // Deletes the last name-email input. First get the count of each-input-div.
   // Will only remove if the count is greater than 2
   
-  if (getShareeDivCount() > 2) {
+  if (getShareeDivs().length > 2) {
     $("#sharee_container div:last-child").remove();
-    $("#sharee-count-label").text(`(${getShareeDivCount()}) chunks`);
+    $("#sharee-count-label").text(`(${getShareeDivs().length}) chunks`);
   }
-  turnMinusButtonONOFF(getShareeDivCount());
+  turnMinusButtonONOFF(getShareeDivs().length);
 }
-function getShareeDivCount() {
+
+// Returns the sharee name-email divs
+function getShareeDivs() {
   let cols = [...$("#sharee_container").find(".each-input-div")];
-  return cols.length;
+  return cols;
 }
+
 function turnMinusButtonONOFF(count) {
   console.log("count line 90", count)
   if (count > 2) {
@@ -97,21 +99,43 @@ function turnMinusButtonONOFF(count) {
   }
 }
 
-function getValidSharers(p_cols) {
+function getValidSharers() {
+  const p_cols = getShareeDivs();
   let sharerArray = []
   let foundGap = false; // if this is true, this function returns an empty array
+  let foundInvalidEmail = false;
   p_cols.forEach((element, count) => {
     const nameValue = element.children[0].value = element.children[0].value.trim();
     const emailValue = element.children[1].value = element.children[1].value.trim();
+    
+    if (!isValidEmail(emailValue)) {
+      foundInvalidEmail = true;
+    }
     if (nameValue !== "" && emailValue !== "") {
       sharerArray.push({ name: nameValue, email: emailValue});
     } else {
       foundGap = true;
     }
-    foundGap ? null : sharerArray;
   })
+  if (foundInvalidEmail) {
+    alert("Invalid or empty email address detected");
+    return;
+  }
+  const finalVal = foundGap ? null : sharerArray;
+  if (finalVal) {
+    // Store in session storage
+    const session = window.sessionStorage;
+    session.setItem('sharees', JSON.stringify(finalVal));
+    console.log(session);
+  } else {
+    alert("Ensure all fields of all rows are filled out, or decrease the amount of rows");
+  }
 }
 
+function isValidEmail(emailAddress) {
+  const patt = /^([\w\-\.]+)@((\[([0-9]{1,3}\.){3}[0-9]{1,3}\])|(([\w\-]+\.)+)([a-zA-Z]{2,4}))$/
+  return patt.test(emailAddress);
+}
 function iterateThroughValues(p_cols) {
   p_cols.forEach((divElement, counter) => {
     console.log(`sharer: ${counter} name: ${divElement.children[0].value} email: ${divElement.children[1].value}`)
