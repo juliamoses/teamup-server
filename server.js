c41b01342cb4cd9d13cdfb543a59e1c489c373d9
Julia Moses <jslmoses@gmail.com>
2019-06-21 16:57:14 -0400

    initial sketeton of local server, with ejs template

---
+++ server.js
@@ -0,0 +1,48 @@
+/////////REQUIRES///////////////////////////
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+/////////APP.USE///////////////////////////
+app.use(express.static('sharerFiles'))
+
+
+/////////APP.SET///////////////////////////
+//how directory will be structured
+//hosting a directory
+//ill have each chunk in folder- assigneed to people
+app.set("view engine", "ejs");
+
+
+
+/////////HELPER FUNCTIONS//////////////////
+
+
+/////////GET REQUESTS/////////////////////(most to least specific)
+//render app
+app.get("/sharerFiles/:fileName", (req, res) => {
+	//point to file location
+  res.render("/");
+});
+
+//
+app.get("/", (req, res) => {
+  res.render("files_form");
+});
+
+
+
+/////////POST REQUESTS/////////////////////(most to least specific)
+
+
+
+
+/////////APP.LISTEN///////////////////////////
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+
+
