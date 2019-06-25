5433adfc8acc8fd3c8abf3463e4ec259d992186d
David Eastmond <davebuck1981@gmail.com>
2019-06-25 11:46:58 -0400

    in server.js , changed the static folder to static

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 
 /////////APP.USE///////////////////////////
-app.use(express.static('sharerFiles'))
+app.use(express.static('static'))
 
 
 /////////APP.SET///////////////////////////
