fb085353ce6b43ba16490d6eda99acd42867eefc
David Eastmond <davebuck1981@gmail.com>
2019-06-28 18:37:36 -0400

    commit before installing socket.io

---
+++ electron/scripts/delegate.js
@@ -0,0 +1,4 @@
+module.exports = function updateDownloadStatus(message) {
+  window.$ = window.jQuery = require('jquery');
+  console.log('delegate function message', message)
+}
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const rootPath = require("electron-root-path").rootPath;
 
+console.log("Line 7 server, ", delegate);
 app.use(express.static('public'))
 
 app.use(express.static('static'))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
 
 app.set("view engine", "ejs");
 
@@ -42,9 +42,6 @@ app.get("/download/:email", (req, res) => {
 	//res.download(sharee.path);
 });
 
-
-
-
 //server will look for file assoiated with email
 //set up test
 app.post("/sharerFiles/:email", (req, res) => {
@@ -75,11 +72,9 @@ app.post("/", (req, res) => {
 
 
 	if (sharee) {
-		res.download(sharee.path, err =>{
-			
+		res.download(sharee.path, (err) =>{
+			//
 		});
-
-		//res.redirect("/download/" + sharee.email)
 	}
 })
 
