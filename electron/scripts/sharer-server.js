03b7e4ea515e5831ab9cd2ab5d5ecda274601eb0
David Eastmond <davebuck1981@gmail.com>
2019-06-28 14:09:14 -0400

    refactored the ajax database insert request to include a promise

--- electron/scripts/sharer-server.js
+++ electron/scripts/sharer-server.js
@@ -2,6 +2,10 @@ window.$ = window.jQuery = require('jquery');
 const internalIp = require('internal-ip');
 const sf = require('split-file');
 
+/* 
+When the sharerserver loads, do an immediate ajax request. If successful, show a status panel
+else, so an error message. Nothing further happens
+*/
 $(document).ready(function (){
   updateLocalIP();
   session = window.sessionStorage;
@@ -11,7 +15,17 @@ $(document).ready(function (){
   // Testdata
   const JSONData = formatDatabaseJSONObject ();
   console.log("JSONDATA", JSONData);
+  let extractedChunks = JSON.parse(JSONData.chunks);
+  console.log("Line 19 extracted chunk data", extractedChunks);
   doAjaxRequest(JSONData)
+  .then((response)=> {
+    console.log("Success", response);
+    // Success - display the download status
+  })
+  .catch((err)=> {
+    $('#upload-section').append(returnErrorMessage("Unable to communicate with remote server."));
+    $('#upload-section').append(returnErrorMessage("Please close the application and try again."));
+  })
 })
 
 function updateLocalIP() {
@@ -23,14 +37,22 @@ function updateLocalIP() {
 }
 
 function doAjaxRequest (data) {
-  //const stringifiedData = JSON.stringify(data);
-  //console.log('Line 28 stringified', stringifiedData)
-  $.post('http://localhost:8081/',data)
+  // Post the file chunk data to the remote server
+  return $.post('http://localhost:8081/',data)
   .then((success)=> {
     console.log(success);
+    return success; // Success is a promise
+  })
+  .catch((err)=> {
+    // Show an error message
+    throw err; // This will cascade to the calling function
   });
 }
 
+function returnErrorMessage(msg) {
+  const errMsg = $('<h5/>', {text: msg}).attr( {id: 'connectionError'}).css('color', 'red');
+  return errMsg;
+}
 function formatDatabaseJSONObject() {
   /**
    * This formats the MongoDB JSON object
